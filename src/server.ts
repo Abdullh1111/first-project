046a07cc8d974765b46fbdbd26db82dc41acfaea
Abdullh1111 <abdullah4474032@gmail.com>
2024-05-23 07:07:45 +0600

    basic set up

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import config from './App/config';
+import app from './app';
+import mongoose from 'mongoose';
+const { port, dataBase_url } = config;
+async function main() {
+  try {
+    await mongoose.connect(dataBase_url as string);
+    app.listen(port, () => {
+      console.log(`Example apps listening on port ${port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+main();
